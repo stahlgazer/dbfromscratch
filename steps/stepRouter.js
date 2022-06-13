3bcc306337fd4fd590b6f183b8bb3dc29a81a675
Gavin S <stahlgavin@gmail.com>
2022-06-13 00:27:56 -0700

    update step by stepID

--- steps/stepRouter.js
+++ steps/stepRouter.js
@@ -15,6 +15,7 @@ router.get("/", (req, res) => {
 // get steps with recipeID
 router.get("/:id", (req, res) => {
   const id = req.params.id;
+
   Steps.getStepsByRecipeId(id)
     .then((data) => res.status(200).json(data))
     .catch((error) => {
@@ -26,17 +27,34 @@ router.get("/:id", (req, res) => {
 
 // create new step with recipeID
 router.post("/:id", (req, res) => {
-  const step = req.body;
   const recipeId = req.params.id;
+  const step = req.body;
+
   Steps.createStep(recipeId, step)
     .then((data) =>
-      res.status(200).json({ message: `new step with id: ${data} created for recipeId: ${recipeId}` })
+      res.status(200).json({
+        message: `new step with id: ${data} created for recipeId: ${recipeId}`,
+      })
     )
     .catch((error) => {
       res
         .status(500)
         .json({ error: `${error.message}, unable to create new step.` });
     });
+});
+
+// update step by stepID
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  Steps.updateByStepId(id, changes)
+    .then((data) => res.status(200).json({ message: `updated step, ${data}` }))
+    .catch((error) =>
+      res
+        .status(500)
+        .json({ error: error.message`unable to update step with id ${id}` })
+    );
 });
 
 module.exports = router;
