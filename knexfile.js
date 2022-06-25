aed0363b125c0ae12ec9767f683e69364e5e64a8
Gavin S <stahlgavin@gmail.com>
2022-06-24 22:00:14 -0700

    testing ssl fix

--- knexfile.js
+++ knexfile.js
@@ -39,7 +39,12 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     migrations: {
       directory: "./database/migrations",
     },
