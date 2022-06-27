3d78b58a4192a63b1c6ee76979d2b3e3c00ce824
Gavin S <stahlgavin@gmail.com>
2022-06-27 14:00:57 -0700

    testing null fix

--- recipes/recipeHelpers.js
+++ recipes/recipeHelpers.js
@@ -61,7 +61,7 @@ async function findbyID(id) {
 
 // creat new recipe
 function create(recipe) {
-  return db("recipe").insert(recipe);
+  return db("recipe").insert(recipe).returning('id');
 }
 
 // update existing recipe by id
